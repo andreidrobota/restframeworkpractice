b20b767d7001ec7c992ba0ae2b75e1846ebce56f
andreidrobota <drobotaandrei@gmail.com>
2024-05-08 15:09:57 +0000

    Added image validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -9,30 +9,26 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image size larger than 2MB!'
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                'Image width larger than 4096px'
-            )
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image heigth larger than 4096px'
+                'Image width larger than 4096px!'
             )
         return value
-    
-    
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-
     class Meta:
-        models = Post
+        model = Post
         fields = [
-            'id','owner', 'is_owner','created_at', 'profile_id',
-            'profile_image', 'updated_at', 'title'
-            'content', 'image', 'image_filter'
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter'
         ]
