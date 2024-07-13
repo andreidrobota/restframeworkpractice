0cdeb678b62fab90e3495f3a8f329dd34000f586
Andrei Drobota <z68sbi67@students.codeinstitute.net>
2024-07-13 08:48:25 +0000

    Modified packacke json and axios

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "https://reactchallenge-b201ad870555.herokuapp.com/"
+// axios.defaults.baseURL = "https://reactchallenge-b201ad870555.herokuapp.com/"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
